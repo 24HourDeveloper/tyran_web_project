a140e45a46bbf6559426eaff1b9f300941187400
Michael Moore <mikeitt009@gmail.com>
2020-04-07 14:24:09 -0400

    first commit

---
+++ lib/src/home/home_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'app-home',
+  templateUrl: 'home_component.html',
+)
+class HomeComponent {
+  // Nothing here yet. All logic is in TodoListComponent.
+}
